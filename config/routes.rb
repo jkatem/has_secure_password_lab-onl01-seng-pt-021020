0a84958ffb9b22359e18e4323b881549b13c340f
Jen Miao <jenmiao@gmail.com>
2020-07-23 17:52:44 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'user/name'
-
-  get 'user/password_digest'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
